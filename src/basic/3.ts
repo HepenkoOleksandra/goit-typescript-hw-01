4547e02ffe677c3662ca92aa68d9b115514813b8
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-05-10 01:29:25 +0300

    generics-4

---
+++ src/basic/3.ts
@@ -0,0 +1,10 @@
+
+let value: string | number;
+value = 3;
+value = "blue";
+value = "false";
+
+type offOrOn = 'enable' | 'disable';
+let position: offOrOn;
+position = 'enable';
+position = "disable";
---
+++ src/generics/4.ts
@@ -0,0 +1,39 @@
+type MyUser = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+// function createOrUpdateUser(initialValues: Partial<MyUser>): MyUser {
+//   const defaultsUser = {
+//     name: "",
+//     surname: "",
+//     email: "",
+//     password: "",
+// }
+
+//   return { ... defaultsUser, ...initialValues };
+// }
+
+
+// createOrUpdateUser({
+//   email: 'user@mail.com',
+//   password: 'password123',
+// });
+
+function createOrUpdateUser(initialValues: MyUser, updateValue: Partial<MyUser>): MyUser {
+    return { ...initialValues, ...updateValue };
+}
+
+const originalUser: MyUser = {
+  name: 'Bob',
+  surname: 'Rob',
+  email: 'bobrob@gmail.com',
+  password: '123654789',
+}
+
+const updateMyUser = createOrUpdateUser(originalUser, { 
+  email: 'user@mail.com', 
+  password: 'password123', 
+});
